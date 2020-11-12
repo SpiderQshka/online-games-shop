3202ca6054c28c76a59adf5368297c12249d8e08
SpiderQshka <prusov.200211@gmail.com>
2020-11-12 16:51:07 +0300

    env final fix (it works!)

--- packages/client/src/config.ts
+++ packages/client/src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  apiUrl: process.env.REACT_APP_URL || process.env.REACT_APP_API_URL,
+  apiUrl: process.env.REACT_APP_HEROKU_API_URL || process.env.REACT_APP_API_URL,
   colors: {
     accent: "#f0810f",
     primaryLight: "#f4f4f4",
